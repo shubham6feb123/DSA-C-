fd30f5e635cddbb513f3f74cdfed05061901a94a
shubham6feb123 <maarahuls555@gmail.com>
2021-09-19 13:51:13 +0530

    DSA BY KUNAL

---
+++ BinarySearch/BinarySearch in 2D Array/BinarySearch__in__2D__array.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+// int BinarySearch_IN_2D_Array(int target){
+
+// };
+int main()
+{
+    int arr[4][4] = {
+        {10,20,30,40},
+        {15,25,35,45},
+        {28,29,37,49},
+        {33,34,38,50}
+    };
+
+    int target = 28;
+    bool ans = false;
+
+    for(int r=0;r<4;r++){
+        int c=0;
+        int s=c;
+        int e = 3;
+        while(s<=e){
+           int m = (s+e)/2;
+           if(arr[r][m]==target){
+               cout<<"Target Found at "<<r<<"--"<<m<<endl;
+               ans = true;
+                break;
+           }
+           if(arr[r][m]>target){
+               e = m-1;
+           }
+
+           if(arr[r][m]<target){
+               s = m+1;
+           }
+
+        }
+    }
+
+ans?cout<<"":cout<<"not found";
+
+    return 0;
+}
